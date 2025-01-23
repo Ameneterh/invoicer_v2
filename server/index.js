a8c1cd7e0d70e023f73fa5ad9d0e5a5151edc5bd
Amene Terhemen <ameneterh@gmail.com>
2025-01-24 00:03:53 +0100

    file name issues resolved

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use("/server/auth", authRouter);
 app.use("/server/business", businessRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dis")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
