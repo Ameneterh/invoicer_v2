e6f581bed6237d1fb6247e3add0536cda835cb8b
Amene Terhemen <ameneterh@gmail.com>
2025-01-25 22:08:21 +0100

    configuring for flowbite-react

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,13 +3,10 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [require("flowbite/plugin")],
-  corePlugins: {
-    preflight: false,
-  },
 };
